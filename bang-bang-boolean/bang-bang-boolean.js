ab0e7d369584472ad7ce9d2f1d57d0b07d6cf4aa
Sahil Sindhu <sahil.sindhu@Sahils-MacBook-Pro.local>
2020-04-11 20:03:21 +0530

    bang-bang-boolean working code

--- bang-bang-boolean/bang-bang-boolean.js
+++ bang-bang-boolean/bang-bang-boolean.js
@@ -4,3 +4,10 @@ let dummyObject = {
 }
 
 let boolean = !!dummyObject;
+
+let section = document.querySelectorAll('section')[0];
+
+let para = document.createElement('p');
+let textNode = document.createTextNode(boolean);
+para.appendChild(textNode);
+section.appendChild(para);

4a2f343f6669ea631485a664e2ef306c4f0311b0
Sahil Sindhu <sahil.sindhu@Sahils-MacBook-Pro.local>
2020-04-21 21:09:00 +0530

    comparision between callback hell and promise

--- promises/script.js
+++ promises/script.js
@@ -1,3 +1,23 @@
+//with callcack hell
+    function doThis(callBack1,callBack2){
+        let val1 =  'do this';
+        callBack1(val1,callBack2);
+    }
+    function thenDothis(result , callcack2){
+        result = result + ' then Do this';
+        callcack2(result);
+    }
+    function andAlsoThis(result){
+        result = result + ' and also this';
+        console.log(result)
+    }
+
+    doThis(thenDothis,andAlsoThis)
+
+
+//
+
+// with promise
 let cleanTheRoom =function(){
    return new Promise(function(resolve,reject){
         resolve('cleaned the room');
@@ -21,7 +41,6 @@ cleanTheRoom().then((result)=>{
     return removeGarbage(result);
 })
 .then((result)=>{
-    console.log(result)
     return getIcecream(result);
 })
 .then((result)=>{

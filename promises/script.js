48c31f5ebc8fbfff0442df0a6d2419f1aa2ff5a4
Sahil Sindhu <sahil.sindhu@Sahils-MacBook-Pro.local>
2020-04-21 20:56:33 +0530

    promises with dependency and preventing callback hell

--- promises/script.js
+++ promises/script.js
@@ -1,19 +1,18 @@
 let cleanTheRoom =function(){
-   return new Promise((resolve,reject)=>{
+   return new Promise(function(resolve,reject){
         resolve('cleaned the room');
-        reject("didn't clean the room")
     })
     
 } 
 
 let removeGarbage = function(message){
-    new Promise((resolve,reject)=>{
+    return new Promise(function(resolve,reject){
         resolve(message + 'removed garbage')
     })
 } 
 
 let getIcecream = function(message){
-    new Promise((resolve,reject)=>{
+    return new Promise(function(resolve,reject){
         resolve(message + 'I got the icecream');
     })
 } 
@@ -22,6 +21,7 @@ cleanTheRoom().then((result)=>{
     return removeGarbage(result);
 })
 .then((result)=>{
+    console.log(result)
     return getIcecream(result);
 })
 .then((result)=>{

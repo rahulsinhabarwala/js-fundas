ffc562a440b76b2dcae8c4cb1e3b4391788d9169
Sahil Sindhu <sahil.sindhu@Sahils-MacBook-Pro.local>
2020-04-26 09:54:08 +0530

    callback hell ::TODOD

--- promises/script.js
+++ promises/script.js
@@ -1,19 +1,4 @@
 //with callcack hell
-    function doThis(callBack1,callBack2){
-        let val1 =  'do this';
-        callBack1(val1,callBack2);
-    }
-    function thenDothis(result , callcack2){
-        result = result + ' then Do this';
-        callcack2(result);
-    }
-    function andAlsoThis(result){
-        result = result + ' and also this';
-        console.log(result)
-    }
-
-    doThis(thenDothis,andAlsoThis)
-
 
 //
 
